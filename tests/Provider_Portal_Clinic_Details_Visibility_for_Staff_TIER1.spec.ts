1d442ee17d9a5b159345fb823a8d45af701ad2b3
IgorEmelyanovPHSA <igor.emelyanov@phsa.ca>
2024-02-15 15:03:57 -0800

    first commit to main

---
+++ tests/Provider_Portal_Clinic_Details_Visibility_for_Staff_TIER1.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://healthbc--hlthbcqax.sandbox.my.site.com/providerclinicportal/s/');
+  await page.goto('https://healthbc--hlthbcqax.sandbox.my.site.com/providerclinicportal/s/login/?ec=302&startURL=%2Fproviderclinicportal%2Fs%2F');
+  await page.getByPlaceholder('Username').click();
+  await page.getByPlaceholder('Username').fill('igor.tier1@phsa.ca.hlthbcqax');
+  await page.locator('#sfdc_password_container div').click();
+  await page.getByPlaceholder('Password').click({
+    modifiers: ['Control']
+  });
+  await page.getByPlaceholder('Password').fill('PAS@1234');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.getByLabel('Search all clinics in BC').click();
+  await page.getByPlaceholder('Enter Clinic Name').click();
+  await page.getByPlaceholder('Enter Clinic Name').fill('cast');
+  await page.getByRole('button', { name: 'Search' }).click();
+  await page.getByText('CASTLEGAR MED ASSOCIATES').click();
+  await page.getByRole('tab', { name: 'Clinic Details' }).click();
+  await page.getByText('COLUMBIA AVE').first().click();
+});
