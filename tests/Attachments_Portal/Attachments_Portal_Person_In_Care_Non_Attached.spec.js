5680c5e09464d8050e4a3ca0ebd5be9fa5467edb
IgorEmelyanovPHSA <igor.emelyanov@phsa.ca>
2024-02-20 14:23:04 -0800

    adding Person_InCare_non_Attched
    adding Person_InCare_non_Attched

---
+++ tests/Attachments_Portal/Attachments_Portal_Person_In_Care_Non_Attached.spec.js
@@ -0,0 +1,36 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://healthbc--hlthbcqax.sandbox.my.site.com/primarycarepatientregistration/s/');
+  await page.getByRole('button', { name: 'Next' }).click();
+  await page.getByRole('button', { name: 'Register a person in my care' }).click();
+  await page.getByRole('combobox', { name: 'Your relationship to this' }).click();
+  await page.getByText('Case manager').click();
+  await page.getByLabel('Your first and last name').click();
+  await page.getByLabel('Your first and last name').fill('Igor Emelyanov CAse MAnager');
+  await page.getByLabel('Your first and last name').press('ArrowLeft');
+  await page.getByLabel('Your first and last name').press('ArrowLeft');
+  await page.getByLabel('Your first and last name').press('ArrowLeft');
+  await page.getByLabel('Your first and last name').press('ArrowLeft');
+  await page.getByLabel('Your first and last name').click();
+  await page.getByLabel('Your first and last name').click();
+  await page.getByLabel('First name', { exact: true }).click();
+  await page.getByLabel('First name', { exact: true }).click();
+  await page.getByLabel('First name', { exact: true }).fill('Kenton');
+    await page.getByLabel('Last name', { exact: true }).click();
+  await page.getByLabel('Last name', { exact: true }).fill('Troup');
+  await page.getByLabel('Personal Health Number (PHN)', { exact: true }).click();
+  await page.getByLabel('Personal Health Number (PHN)', { exact: true }).click();
+  await page.getByLabel('Personal Health Number (PHN)', { exact: true }).fill('9873010088');
+  await page.locator('#content-167').click();
+  await page.getByPlaceholder('MM').click();
+  await page.getByPlaceholder('MM').fill('12');
+  await page.getByPlaceholder('DD').dblclick();
+  await page.getByPlaceholder('DD').fill('05');
+  await page.getByPlaceholder('YYYY').dblclick();
+  await page.getByPlaceholder('YYYY').fill('1959');
+  await page.getByPlaceholder('DD').click();
+  await page.getByRole('button', { name: 'Continue' }).click();
+  await page.getByRole('button', { name: 'Continue' }).click();
+  
+});
