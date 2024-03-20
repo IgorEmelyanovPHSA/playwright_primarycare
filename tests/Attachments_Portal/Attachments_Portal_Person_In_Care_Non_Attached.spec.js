eb8bdc3fc832dee6456e5db920ad86ff5fd8a3b0
IgorEmelyanovPHSA <igor.emelyanov@phsa.ca>
2024-03-20 11:17:37 -0700

    remove the way around issue Step
    remove the way around issue Step

--- tests/Attachments_Portal/Attachments_Portal_Person_In_Care_Non_Attached.spec.js
+++ tests/Attachments_Portal/Attachments_Portal_Person_In_Care_Non_Attached.spec.js
@@ -1,31 +1,27 @@
 import { test, expect } from '@playwright/test';
 import { constants } from 'buffer';
 
-test('Can_do_Person_InCare_Registration_for_NoN_Attached_in_Portal', async ({ page }) => {
+test('Can_do_Person_InCare_Registration_for_NoN_Attached_in_Portal', async ({ page, browser }) => {
   
-  //console.log("\n=====Devtools : enable Performance Metrics =====\n");
+  console.log("\n=====Devtools : enable Performance Metrics =====\n");
   //Create a new connection to an eisting CDPSession to enable Performance Mesurements
-  //const client = await page.context().newCDPSession(page);
+  const client = await page.context().newCDPSession(page);
   //Tell the DevTools session to record Performance Metrics
-  //await client.send('Performance.enable');
+  await client.send('Performance.enable');
 
-  //const { chromium } = require('playwright');  // Or 'chromium' or 'webkit'.
-
-  //(async () => {
-    //const browser = await chromium.launch();
-    //const page = await browser.newPage();
-    //await page.goto('https://example.com');
-  //await browser.close();
-  //});
-
-  //console.log("\n=====Devtools : startTracing =====\n");
-  //await browser.startTracing(page, {path: '.trace.json', sceenshots: true});
   
+
+  console.log("\n=====Devtools : startTracing =====\n");
+  await browser.startTracing(page, {path: 'trace.json', sceenshots: true});
   await page.goto('https://healthbc--hlthbcqax.sandbox.my.site.com/primarycarepatientregistration/s/');
-
-  //START - Use Performance.mark API
+  ///////START - Use Performance.mark API
   await page.evaluate(() => window.performance.mark("perf:start"));
-
+  ///////
+  ///FCP - First Content Paint tracing
+  //const paintTimingJson = await page.evaluate(() => JSON.stringify(window.performance.getEntriesByType('paint')))
+  //const paintTiming = JSON.parse(paintTimingJson)
+  //console.log(paintTiming)
+  //////
   await page.getByRole('button', { name: 'Next' }).click();
   await page.getByRole('button', { name: 'Register a person in my care' }).click();
   await page.getByRole('combobox', { name: 'Your relationship to this' }).click();
@@ -54,27 +50,51 @@ test('Can_do_Person_InCare_Registration_for_NoN_Attached_in_Portal', async ({ pa
   await page.getByPlaceholder('DD').click();
   await page.getByLabel('Personal Health Number (PHN)', { exact: true }).click();
   await page.getByRole('button', { name: 'Continue' }).click();
-  
+
+  console.log("\n=====The Paint Timing =====\n");
+  ///FCP - First Content Paint tracing
+  const paintTimingJson = await page.evaluate(() => JSON.stringify(window.performance.getEntriesByType('paint')))
+  const paintTiming = JSON.parse(paintTimingJson)
+  console.log(paintTiming)
+  ///LCP 
+  const largestContentfulPaint = await page.evaluate(() => {
+    return new Promise((resolve) => {
+      new PerformanceObserver((l) => {
+        const entries = l.getEntries()
+        // the last entry is the largest contentful paint
+        const largestPaintEntry = entries.at(-1)
+        resolve(largestPaintEntry.startTime)
+      }).observe({
+        type: 'largest-contentful-paint',
+        buffered: true
+      })
+    })
+  })
+  console.log("\n largestContentfulPaint =", parseFloat(largestContentfulPaint))
+  /////////////////////////////////////
+
+
+  console.log("\n=====Devtools : Performance.getMetricd =====\n");
+  let performanceMetrics = await client.send('Performance.getMetrics');
+  console.log(performanceMetrics.metrics);
+
+  console.log("\n=====Devtools : Get Tracing =====\n");
   //Performance.mark API - STOP
-  await page.evaluate(() => window.performance.mark("perf:stop"));
   
+  await page.evaluate(() => window.performance.mark("perf:stop"));
   //Performance.measure API
   await page.evaluate(() => window.performance.measure("overall","perf:start","perf:stop"));
-
-  //Get All Performance Marks Includinh Google'e
-  const getAllMarksJson = await page.evaluate(() => JSON.stringify(window.performance.getEntriesByType("measure")));
+  //Get All Performance Marks Including Google'e
+  const getAllMarksJson = await page.evaluate(() => JSON.stringify(window.performance.getEntriesByType("mark")));
   const getAllMarks = JSON.parse(getAllMarksJson);
-  console.log('window.performance.getEntriesByType("measure")', getAllMarks);
-
-  //Get All Performance Measures Includinh Google'e
+  console.log('window.performance.getEntriesByType("mark")', getAllMarks);
+  //Get All Performance Measures Including Google'e
   const getAllMeasureJson = await page.evaluate(() => JSON.stringify(window.performance.getEntriesByType("measure")));
-  const getAllMeasures = JSON.parse(getAllMarksJson);
+  const getAllMeasures = JSON.parse(getAllMeasureJson);
   console.log('window.performance.getEntriesByType("measure")', getAllMeasures);
 
+  console.log("\n=====Devtools : stopTracing =====\n");
+  await browser.stopTracing();
 
-
-  //console.log("\n=====Devtools : Performance.getMetricd =====\n");
-  //et performanceMetrics = await client.send('Performance.getMetrics');
-  //onsole.log(performanceMetrics.metrics);
 
 });
