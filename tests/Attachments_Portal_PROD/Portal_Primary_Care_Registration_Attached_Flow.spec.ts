e9ff64d3698c5906523b1d3cdb854d23f323cffa
IgorEmelyanovPHSA <igor.emelyanov@phsa.ca>
2024-07-09 08:37:17 -0700

    PROD: July 09 updates for Heakth Questions
    High Blood Sugar (for example, Diabetes)

--- tests/Attachments_Portal_PROD/Portal_Primary_Care_Registration_Attached_Flow.spec.ts
+++ tests/Attachments_Portal_PROD/Portal_Primary_Care_Registration_Attached_Flow.spec.ts
@@ -98,11 +98,12 @@ test('Can_Register_Person_In_My_Care_LAWERENCE_MENZIES_for_Attached_in_Portal', 
   await page.getByRole('button', { name: 'Continue' }).click();
   await page.locator('label').filter({ hasText: 'I am pregnant or have' }).locator('span').first().click();
   await page.locator('label').filter({ hasText: 'Seizures or Epilepsy' }).locator('span').first().click();
+  await page.locator('label').filter({ hasText: 'High Blood Sugar (for example, Diabetes)' }).locator('span').first().click();
   await page.getByRole('button', { name: 'Continue' }).click();
-  ///await page.getByRole('button', { name: 'Submit registration' }).click();
-  ///await page.getByText('Lawerence Menzies is now on the Health Connect Registry.').click();
-  ///await page.getByRole('heading', { name: 'Successfully registered!' }).click();
-  ///await page.getByRole('button', { name: 'Start a new registration' }).click();
+  //await page.getByRole('button', { name: 'Submit registration' }).click();
+  //await page.getByText('Lawerence Menzies is now on the Health Connect Registry.').click();
+  //await page.getByRole('heading', { name: 'Successfully registered!' }).click();
+  //await page.getByRole('button', { name: 'Start a new registration' }).click();
  */
 }); 
 
