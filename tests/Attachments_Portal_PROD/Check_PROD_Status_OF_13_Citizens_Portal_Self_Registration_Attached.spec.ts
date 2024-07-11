5fd180bf32dac928db76346d72b16115e037cd87
IgorEmelyanovPHSA <igor.emelyanov@phsa.ca>
2024-07-11 13:55:41 -0700

    Prod: July 01 after  the Last Deploy

--- tests/Attachments_Portal_PROD/Check_PROD_Status_OF_13_Citizens_Portal_Self_Registration_Attached.spec.ts
+++ tests/Attachments_Portal_PROD/Check_PROD_Status_OF_13_Citizens_Portal_Self_Registration_Attached.spec.ts
@@ -225,8 +225,9 @@ test('Can_do_Huey_Cheng_Registration_for_Attached_in_Portal', async ({page, brow
 
     //await page.getByText('Thank you, Huey Cheng is already registered.').click();
     //AI Click on the "There was an issue with the information entered." text
-    //await page.click('omnistudio-omniscript-text-block[data-omni-key="TextBlock4"] h1.head');
-    
+    await page.click('omnistudio-omniscript-text-block[data-omni-key="TextBlock4"] h1.head');
+
+    /*
     await page.getByRole('combobox', { name: 'Street address' }).click();
     await page.getByRole('combobox', { name: 'Street address' }).fill('307-7631 Francis Rd');
     await page.getByRole('textbox', { name: 'City' }).click();
@@ -301,7 +302,7 @@ test('Can_do_Huey_Cheng_Registration_for_Attached_in_Portal', async ({page, brow
     ////await page.getByRole('article').click();
     ////await page.getByText('Each community has a team').click();
     ////await page.getByRole('heading', { name: 'Successfully registered!' }).click();
-    
+    */
 });
 
 test('Can_do_Courtney_Harty_Self_Registration_for_Attached_in_Portal', async ({page, browser }) => {
